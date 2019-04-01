006a0284bcba9152b2c1f75a5a655fde028ecc75
ywu25 <yifei_wu1@brown.edu>
2019-04-01 13:37:37 -0400

    Fix app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, reqparse, Api
 from resources.Contacts import ContactAll
 
 application = Flask(__name__)
-api = Api(app)
+api = Api(application)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://skflpscmrdpxig:b5d9f1887753ae2dedf55325f7253f053a46388c685fa288ff1d4469b15fabe1@ec2-23-21-128-35.compute-1.amazonaws.com:5432/d8i4u0mq57b2cv'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 application.config['PROPAGATE_EXCEPTIONS'] = True
