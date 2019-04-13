5d28084a98088e78fd4ff6d2efc126c59c2c3e3f
Paarul Sukanya <paarulsukanya@Paaruls-MacBook-Pro.local>
2019-04-13 15:48:00 -0400

    404 error

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -4,22 +4,6 @@ from flask import url_for
 def test_add_contact(app):
     # print(app)
     with app.test_client() as client:
-        response =client.get(url_for('contacts'), follow_redirects=True)
-        # response = client.post('/contacts',
-        #                         data=json.dumps(dict(
-        #                         first_name= "abc",
-        #                         last_name= "xyz",
-        #                         email_primary= "p@gmail.com",
-        #                         phone_primary="111-111-1111",
-        #                         gender= "Female",
-        #                         race_all= "Asian",
-        #                         birthdate= "2012-04-23"
-        #                         )),
-        #                         content_type='application/json'
-        #                        )
-        #data = json.loads(response.data.decode())
+        response =client.get('/contacts/')
 
-        assert response.json =={
-                                    "status": "success",
-                                    "data": []
-                                }
+        assert response.status_code == 200
