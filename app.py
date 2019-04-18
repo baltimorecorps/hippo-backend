1612f59515cd4c9c1800145535ff935dc650c99e
ywu25 <yifei_wu1@brown.edu>
2019-04-18 19:43:40 -0400

    Fix indentation

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def create_app(config_filename):
 
     app.register_blueprint(api_bp, url_prefix='/api')
 
-	@app.route('/')
-	def home_page():
-	    return 'Home page'
+    @app.route('/')
+    def home_page():
+    	return 'Home page'
 
     from models.base_model import db
     db.init_app(app)
