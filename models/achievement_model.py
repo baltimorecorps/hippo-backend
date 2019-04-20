373d2fc387135c4c7868e8cc626b5e39e193bd4d
Aylin Perez <aylin_pere@brown.edu>
2019-04-20 03:00:24 -0400

    Add achievement POST, PUT, DELETE

--- models/achievement_model.py
+++ models/achievement_model.py
@@ -5,6 +5,7 @@ from marshmallow import Schema, fields
 class Achievement(db.Model):
     __tablename__ = "achievement"
     id = db.Column(db.Integer, primary_key=True)
+    contact_id = db.Column(db.Integer, nullable = False)
     exp_id = db.Column(db.Integer, db.ForeignKey("experience.id"), nullable=False)
     description = db.Column(db.String(500))
     achievement_order = db.Column(db.Integer)
@@ -13,6 +14,7 @@ class Achievement(db.Model):
 
 class AchievementSchema(Schema):
     id = fields.Integer()
+    contact_id = fields.Integer()
     exp_id = fields.Integer()
     description = fields.String()
     achievement_order = fields.Integer()
