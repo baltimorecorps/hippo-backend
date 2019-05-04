9f068c66d59458bca69b8dedf3f77beafca457a2
Billy Daly <william.daly@mail.goucher.edu>
2019-05-04 10:26:55 -0700

    Refactors achievements resource file for additional clarity

--- resources/Achievement.py
+++ resources/Achievement.py
@@ -9,17 +9,13 @@ class AchievementsAll(Resource):
 
     def post(self, experience_id):
         json_data = request.get_json(force=True)
-
         if not json_data:
             return {'message': 'No input data provided'}, 400
-
         # Validate and deserialize input
         data, errors = achievement_schema.load(json_data)
         if errors:
             return errors, 422
-
         achievement = Achievement(**data)
-
         db.session.add(achievement)
         db.session.commit()
         result = achievement_schema.dump(achievement).data
@@ -28,7 +24,7 @@ class AchievementsAll(Resource):
 class AchievementOne(Resource):
 
     def delete(self, achievement_id):
-        achievement = Achievement.query.filter_by(id=achievement_id)
+        achievement = Achievement.query.get(achievement_id)
         if not achievement.first():
             return {'message': 'Achievement does not exist'}, 400
         achievement.delete()
@@ -36,16 +32,16 @@ class AchievementOne(Resource):
         return {"status": 'success'}, 201
 
     def put(self, achievement_id):
-        achievement = Achievement.query.filter_by(id=achievement_id)
-        if not achievement.first():
+        achievement = Achievement.query.get(achievement_id)
+        if not achievement:
             return {'message': 'Achievement does not exist'}, 400
         json_data = request.get_json(force=True)
         # Validate and deserialize input
         data, errors = achievement_schema.load(json_data)
         if not data:
             return {'message': 'No data provided to update'}, 400
-
-        # ach = Achievement(**data)
-        achievement.update(data)
+        for k,v in data.items():
+            setattr(exp, k, v)
         db.session.commit()
-        return {"status": 'success'}, 201
+        result = experience_schema.dump(exp).data
+        return {"status": 'success', 'data': result}, 201
