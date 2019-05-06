d167ab10726f8d4742d0bedbdf39a573e9e6cb38
Billy Daly <william.daly@mail.goucher.edu>
2019-05-06 00:16:16 -0400

    Refactors resume resource and updates the api file with the new endpoint names

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from resources.Contacts import ContactAll, ContactOne
 from resources.Tag import TagAll, TagOne, TagItemAll, TagItemOne
 from resources.Experience import ExperienceAll, ExperienceOne
-from resources.Achievement import AchievementsAll, AchievementOne
-from resources.Resume import ContactsResume, ResumeOne, ResumeSections
+from resources.Achievement import AchievementAll, AchievementOne
+from resources.Resume import ResumeAll, ResumeOne, ResumeSectionAll
 
 api_bp = Blueprint('api',__name__)
 api = Api(api_bp)
@@ -35,18 +35,24 @@ api.add_resource(TagItemAll,
 api.add_resource(TagItemOne,
                  '/contacts/<int:contact_id>/tags/<int:tag_id>',
                  '/contacts/<int:contact_id>/tags/<int:tag_id>/')
-api.add_resource(AchievementsAll,
+api.add_resource(AchievementAll,
                  '/experiences/<int:experience_id>/achievements/',
                  '/experiences/<int:experience_id>/achievements')
 api.add_resource(AchievementOne,
                  '/achievements/<int:achievement_id>/',
                  '/achievements/<int:achievement_id>')
-api.add_resource(ContactsResume,
+api.add_resource(ResumeAll,
                  '/contacts/<int:contact_id>/resumes/',
                  '/contacts/<int:contact_id>/resumes')
 api.add_resource(ResumeOne,
                  '/resumes/<int:resume_id>/',
                  '/resumes/<int:resume_id>')
-api.add_resource(ResumeSections,
+api.add_resource(ResumeSectionAll,
                  '/resumes/<int:resume_id>/sections/',
                  '/resumes/<int:resume_id>/sections')
+api.add_resource(ResumeItemAll,
+                 '/resumes/<int:resume_id>/sections/<int:section_id>/items/',
+                 '/resumes/<int:resume_id>/sections/<int:section_id>/items')
+api.add_resource(ResumeItemOne,
+                 '/resumes/<int:resume_id>/sections/<int:section_id>/items/<int:item_position>/',
+                 '/resumes/<int:resume_id>/sections/<int:section_id>/items/<int:item_position>')
--- resources/Resume.py
+++ resources/Resume.py
@@ -1,45 +1,49 @@
 from flask_restful import Resource, request
 from models.resume_model import Resume, ResumeSchema
+from models.resume_section_model import ResumeSection, ResumeSectionSchema
+from models.resume_item_model import ResumeItem, ResumeItemSchema
 
 resumes_schema = ResumeSchema(many=True)
 resume_schema = ResumeSchema(exclude=['sections'])
 resume_render_schema = ResumeSchema()
 
+resume_sections_schema = ResumeSectionSchema(many=True)
+resume_section_schema = ResumeSectionSchema()
 
-class ContactsResume(Resource):
+resume_items_schema = ResumeItemSchema(many=True)
+resume_item_schema = ResumeItemSchema()
+
+class ResumeAll(Resource):
 
     def get(self, contact_id):
         res = Resume.query.filter_by(contact_id=contact_id)
         res_list = resumes_schema.dump(res).data
-
         return {'status': 'success', 'data': res_list}, 200
 
     def post(self, contact_id):
         json_data = request.get_json(force=True)
-
-        if not json_data:
-            return {'message': 'No input data provided'}, 400
-
-        # Validate and deserialize input
         data, errors = resume_schema.load(json_data)
+        if not data:
+            return {'message': 'No input data provided'}, 400
         if errors:
             return errors, 422
-
         res = Resume(**data)
-
         db.session.add(res)
         db.session.commit()
         result = resume_schema.dump(res).data
-
         return {'status': 'success', 'data': result}, 201
 
 class ResumeOne(Resource):
 
     def get(self, resume_id):
-        pass
+        res = Resume.query.get(resume_id)
+        if not res:
+            return {'message': 'Resume does not exist'}, 400
+        result = resume_render_schema.dump(res).data
+        return {'status': 'success', 'data': result}, 200
 
     def delete(self, resume_id):
-        res = Resume.query.filter_by(id=resume_id)
+        res = Resume.query.get(resume_id)
         if not res.first():
             return {'message': 'Resume does not exist'}, 400
         res.delete()
@@ -47,18 +51,93 @@ class ResumeOne(Resource):
         return {'status': 'success'}, 201
 
     def put(self, resume_id):
-        res = Resume.query.filter_by(id=resume_id)
-        if not res.first():
+        res = Resume.query.get(resume_id)
+        if not res:
             return {'message': 'Resume does not exist'}, 400
         json_data = request.get_json(force=True)
         data, errors = resume_schema.load(json_data)
         if not data:
             return {'message': 'No data provided to update'}, 400
-        res.update(data)
+        if errors:
+            return errors, 422
+        for k,v in data.items():
+            setattr(res, k, v)
         db.session.commit()
-        return {'status': 'success'}, 201
+        result = resume_schema.dump(res)
+        return {'status': 'success', 'data': result}, 201
 
-
-class ResumeSections(Resource):
+class ResumeSectionAll(Resource):
     def get(self, resume_id):
+        sections = ResumeSection.query.filter(resume_id=resume_id)
+        res_list = resume_sections_schema.dump(res).data
+        return {'status': 'success', 'data': res_list}, 200
+
+    def post(self, resume_id):
+        json_data = request.get_json(force=True)
+        data, errors = resume_section_schema.load(json_data)
+        if not data:
+            return {'message': 'No input data provided'}, 400
+        if errors:
+            return errors, 422
+        section = ResumeSection(**data)
+        db.session.add(section)
+        db.session.commit()
+        result = resume_schema.dump(section).data
+        return {'status': 'success', 'data': result}, 201
+
+class ResumeSectionOne(Resource):
+
+    def get(self, section_id):
+        section = ResumeSection.query.get(section_id)
+        if not section:
+            return {'message': 'Resume section does not exist'}, 400
+        result = resume_section_schema.dump(section)
+        return {'status': 'success', 'data': result}, 201
+
+class ResumeItemAll(Resource):
+
+    def post(self, section_id):
+        json_data = request.get_json(force=True)
+        data, errors = resume_item_schema.load(json_data)
+        if not data:
+            return {'message': 'No input data provided'}, 400
+        if errors:
+            return errors, 422
+        item = ResumeItem(**data)
+        db.session.add(item)
+        db.session.commit()
+        result = resume_item_schema.dump(section).data
+        return {'status': 'success', 'data': result}, 201
+
+class ResumeItemOne(Resource):
+
+    def put(self, section_id, item_position):
+        item = ResumeItem.query.filter_by(section_id=section_id,
+                                          resume_order=item_position).first()
+        if not item:
+            return {'message': 'Item does not exist'}
+        json_data = request.get_json(force=True)
+        data, errors = resume_schema.load(json_data)
+        if not data:
+            return {'message': 'No data provided to update'}, 400
+        if errors:
+            return errors, 422
+        for k,v in data.items():
+            setattr(item, k, v)
+        db.session.commit()
+        result = resume_item_schema.dump(item)
+        return {'status': 'success', 'data': result}, 201
+
+class ResumeItemReorder(Resource):
+
+    def patch(self, section_id, item_position, new_position):
         pass
+        '''
+        Not sure if it would make sense to implement an endpoint like this
+        but I was thinking this could be used to efficiently update
+        the order of the items in a section by following the strategy
+        suggested in this post:
+        https://softwareengineering.stackexchange.com/questions/195308/storing-a-re-orderable-list-in-a-database
+        for the time being, this same functionality can be supported by
+        simply PUTting to each of the items in the section
+        '''
