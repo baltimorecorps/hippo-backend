a12c3d0f09e2f1d326daef0a72a1eab38bfbe298
David Koh <david.f.koh@gmail.com>
2019-08-29 16:26:19 -0500

    Added tests for all API GET requests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,31 +1,32 @@
 import json
+from pprint import pprint
 import pytest
 
 """
 * GET api/contacts/
+* GET api/contacts/<int:contact_id>/
+* GET api/contacts/<contact_id>/experiences/
+* GET api/experiences/<int:exp_id>/
+* GET api/tags/
+* GET api/tags/<tag_id>/
+* GET api/contacts/<int:contact_id>/achievements/
+* GET api/contacts/<contact_id>/tags/
+* GET api/contact/<contact_id>/resumes/
+* GET api/resumes/<int:resume_id>/
+* GET api/resumes/<resume_id>/sections/
+GET api/resumes/<resume_id>/sections/<section_id>/
+
 POST api/contacts/
-GET api/contacts/<int:contact_id>/
-GET api/contacts/<int:contact_id>/
-GET api/contacts/<contact_id>/experiences/
 POST api/contacts/<contact_id>/experiences/
-GET api/experiences/<int:exp_id>/
 PUT api/experiences/<int:exp_id>/
 DELETE api/experiences/<int:exp_id>/
-GET api/tags/
 POST api/tags/
-GET api/tags/<tag_id>/
-GET api/contacts/<contact_id>/tags/
 POST api/contacts/<contact_id>/tags/
 PUT api/contacts/<contact_id>/tags/<tag_item_id>/
-GET api/contacts/<int:contact_id>/achievements/
-GET api/contact/<contact_id>/resumes/
 POST api/contacts/<contact_id>/resumes/
-GET api/resumes/<int:resume_id>/
 PUT api/resumes/<resume_id>/
 DELETE api/resumes/<resume_id>/
-GET api/resumes/<resume_id>/sections/
 POST api/resumes/<resume_id>/sections/<section_id>/
-GET api/resumes/<resume_id>/sections/<section_id>/
 PUT api/resumes/<resume_id>/sections/<section_id>/
 DELETE api/resumes/<int:section_id>/sections/<section_id>/
 """
@@ -76,34 +77,163 @@ CONTACTS = {
     }
 }
 
+ACHIEVEMENTS = {
+    'baltimore1': {
+        'id': 81,
+        'description': 'Redesigned the Salesforce architecture to facilitate easier reporting.',
+    },
+    'baltimore2': {
+        'id': 82,
+        'description': 'Formalized organizational strategy for defining and analyzing KPIs.',
+    },
+    'baltimore3': {
+        'id': 83,
+        'description': 'Developed recruitment projection tools to model and track progress to goals.',
+    },
+    'goucher1': {
+        'id': 84,
+        'description': 'Did some stuff',
+    }
+}
+
+EXPERIENCES = {
+    'columbia': {
+        'id': 511,
+        'description': None,
+        'host': 'Columbia University',
+        'title': 'Political Science',
+        'degree': 'Undergraduate',
+        'date_start': '1979-09-03',
+        'date_end': '1983-05-22',
+        'type': 'Education',
+        'contact_id': 124,
+        'achievements': [],
+    },
+    'goucher': {
+        'id': 512,
+        'description': None,
+        'host': 'Goucher College',
+        'title': 'Economics',
+        'degree': 'Undergraduate',
+        'date_start': '2012-09-01',
+        'date_end': '2016-05-20',
+        'type': 'Education',
+        'contact_id': 123,
+        'achievements': [
+            ACHIEVEMENTS['goucher1'],
+        ],
+    },
+    'baltimore' : {
+        'id': 513,
+        'description': 'Test description here',
+        'host': 'Baltimore Corps',
+        'title': 'Systems Design Manager',
+        'degree': 'Undergraduate',
+        'date_start': '2000-01-01',
+        'date_end': '2019-07-17',
+        'type': 'Work',
+        'contact_id': 123,
+        'achievements': [
+            ACHIEVEMENTS['baltimore1'],
+            ACHIEVEMENTS['baltimore2'],
+            ACHIEVEMENTS['baltimore3'],
+        ],
+    },
+}
+
 TAGS = {
     'python': {
         'id': 123,
         'name': 'Python',
         'type': 'Skill',
-        'status': 'Active',
     },
     'webdev': {
         'id': 124,
         'name': 'Web Development',
         'type': 'Function',
-        'status': 'Active',
     },
     'health': {
         'id': 125,
         'name': 'Public Health',
         'type': 'Topic',
-        'status': 'Active',
+    },
+}
+
+TAG_ITEMS = {
+    'billy_webdev': {
+        'id': 21,
+        'name': 'Web Development',
+        'type': 'Function',
+        'contact_id': 123,
+        'tag_id': 124,
+        'score': 2,
     }
 }
 
+# This is kind of gross -- maybe we should consider standardizing the resume
+# responses so that they're the same as everything else?
+def filter_dict(d, keys):
+    return {k:v for k, v in d.items() if k not in keys}
 
+RESUME_SECTIONS = {
+    'billy_work': {
+        'id': 61,
+        'resume_id': 51,
+        'max_count': None,
+        'min_count': None,
+        'name': "Work Experience",
+        'items': [
+            {
+                'resume_order': 0,
+                'indented': False,
+                'achievement': None,
+                'tag': None,
+                'experience': filter_dict(EXPERIENCES['baltimore'], 
+                                          {'achievements', 'contact_id'}),
+            },
+        ],
+    },
+    'billy_skills': {
+        'id': 62,
+        'resume_id': 51,
+        'max_count': None,
+        'min_count': None,
+        'name': "Skills",
+        'items': [
+            {
+                'resume_order': 0,
+                'indented': False,
+                'achievement': None,
+                'experience': None,
+                'tag': filter_dict(TAG_ITEMS['billy_webdev'], {'contact_id'}),
+            },
+        ],
+    },
+}
 
+RESUMES = {
+    'billy': {
+        'id': 51,
+        'contact': CONTACTS['billy'],
+        'name': "Billy's Resume",
+        'date_created': '2019-05-04',
+        'sections': [
+            filter_dict(RESUME_SECTIONS['billy_work'], {'resume_id'}),
+            filter_dict(RESUME_SECTIONS['billy_skills'], {'resume_id'}),
+        ],
+    },
+}
 
 @pytest.mark.parametrize(
     "url,expected",
     [('/api/contacts/123/', CONTACTS['billy'])
     ,('/api/contacts/124/', CONTACTS['obama'])
+    ,('/api/experiences/512/', EXPERIENCES['goucher'])
+    ,('/api/experiences/513/', EXPERIENCES['baltimore'])
+    ,('/api/tags/123/', TAGS['python'])
+    ,('/api/tags/124/', TAGS['webdev'])
+    ,('/api/resumes/51/', RESUMES['billy'])
+    ,('/api/resumes/51/sections/61/', RESUME_SECTIONS['billy_work'])
     ]
 )
 def test_get(app, url, expected):
@@ -114,12 +244,42 @@ def test_get(app, url, expected):
     }
     with app.test_client() as client:
         response = client.get(url, headers=headers)
-        print(response)
         assert response.status_code == 200
         data = json.loads(response.data)['data']
         assert len(data) > 0
-        print(json.loads(response.data))
         assert data == expected
+
+@pytest.mark.parametrize(
+    "url,expected",
+    [('/api/contacts/', CONTACTS.values())
+    ,('/api/contacts/123/experiences/', [EXPERIENCES['goucher'], 
+                                         EXPERIENCES['baltimore']])
+    ,('/api/contacts/124/experiences/', [EXPERIENCES['columbia']])
+    ,('/api/contacts/123/achievements/', ACHIEVEMENTS.values())
+    ,('/api/tags/', TAGS.values())
+    ,('/api/contacts/123/tags/', TAG_ITEMS.values())
+    ,('/api/contacts/123/resumes/', RESUMES.values())
+    ,('/api/resumes/51/sections/', RESUME_SECTIONS.values())
+    ]
+)
+def test_get_many_unordered(app, url, expected):
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+    with app.test_client() as client:
+        response = client.get(url, headers=headers)
+        assert response.status_code == 200
+        data = json.loads(response.data)['data']
+
+        # Test that the data and expected contain the same items, but not
+        # necessarily in the same order
+        assert len(data) == len(expected)
+        pprint(list(expected))
+        for item in data:
+            pprint(item)
+            assert item in expected
 
 
 def est_post_one_contact(app):
