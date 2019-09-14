57755e30a706c6a01e2070cd09ec4a884a8e89bf
David Koh <david.f.koh@gmail.com>
2019-09-14 12:10:52 -0500

    Add ability to have a 'dev' environment

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import os
 from flask import Flask
 from api import api_bp
 
+def load_from_dev(app):
+    app.config.from_pyfile('secrets/dev.cfg')
+
+def load_config(app):
+    if os.environ.get('DEPLOY_ENV') == 'dev':
+        load_from_dev(app)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('defaultcfg')
+    load_config(app)
     app.register_blueprint(api_bp, url_prefix='/api')
 
     @app.route('/')
