a8187d9482ed64a2ac7782486f45e61aa56beecf
Billy Daly <william.daly@mail.goucher.edu>
2020-01-19 12:34:48 -0500

    Updates how POST to IntakeTalentCard resource works: - Creates a new add_new_talent_card() function - Replaces the code in the POST method with this new function

--- resources/Trello_Intake_Talent.py
+++ resources/Trello_Intake_Talent.py
@@ -19,6 +19,30 @@ def get_intake_talent_board_id(program_id):
 
 BOARD_ID = '5ddd741f5cc43e2b21346dbb'
 
+def add_new_talent_card(program_id, contact_id):
+    board_id = get_intake_talent_board_id(program_id)
+    program_contact = query_one_program_contact(contact_id, program_id)
+    contact = program_contact.contact
+    email = contact.email_primary.email
+
+    board_data = query_board_data(BOARD_ID)
+    board = Board(board_data)
+    started_list = board.lists['stage'][1]
+    existing_card = board.find_card_by_custom_field('Email', email)
+    fields_data = {
+        'Phone': contact.phone_primary,
+        'Email': email,
+        'External ID':contact.id
+    }
+    if existing_card:
+        result = existing_card.move_card(started_list)
+    else:
+        card_data = {
+            'name': f'{contact.first_name} {contact.last_name}'
+        }
+        new_card = started_list.add_card_from_template(**card_data)
+    return result
+
 class IntakeTalentBoard(Resource):
 
     def get(self, program_id):
@@ -46,13 +70,5 @@ class IntakeTalentCard(Resource):
         return {'status': 'success', 'data': data}, 200
 
     def post(self, contact_id, program_id):
-        board_id = get_intake_talent_board_id(program_id)
-        program_contact = query_one_program_contact(contact_id, program_id)
-        contact = program_contact.contact
-
-        data = query_board_data(BOARD_ID)
-        board = Board(data)
-        started_list = board.lists['stage'][1]
-        data = {'name': f'{contact.first_name} {contact.last_name}'}
-        new_card = started_list.add_card_from_template(**data)
-        return {'status': 'success', 'data': new_card.list.id}, 201
+        result = add_new_talent_card(contact_id, program_id)
+        return {'status': 'success', 'data': result}, 201
