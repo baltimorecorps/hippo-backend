ad46592b46a5342e6b5a1ec5b9b56694db27b90f
Billy Daly <william.daly@mail.goucher.edu>
2020-02-03 09:25:47 -0500

    Updates review_model.py:
    - removes reviewer col
    - adds is_active col
    - adds program_contact relationship
    - creates ReviewSchema class

--- models/review_model.py
+++ models/review_model.py
@@ -2,13 +2,28 @@ from models.base_model import db
 from marshmallow import Schema, fields, post_dump, EXCLUDE
 
 class Review(db.Model):
+    __tablename__='review'
 
     #table columns
     id = db.Column(db.Integer, nullable=False, primary_key=True)
-    reviewer = db.Column(db.String(50))
     score = db.Column(db.Integer)
     stage = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean, default=True)
     card_id = db.Column(db.String(25))
     program_contact_id = db.Column(db.Integer,
                                    db.ForeignKey('program_contact.id'),
                                    nullable=False)
+
+    #relationships
+    program_contact = db.relationship('ProgramContact',
+                                      back_populates='reviews')
+
+class ReviewSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    is_active = fields.Boolean()
+    stage = fields.Integer()
+    card_id = fields.String()
+    score = fields.Integer()
+
+    class Meta:
+        unknown = EXCLUDE
