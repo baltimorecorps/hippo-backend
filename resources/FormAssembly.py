50da66b24f9b48f9756469adffdbe29085371058
Billy Daly <william.daly@mail.goucher.edu>
2020-02-17 17:46:54 -0500

    Adds Trello integration to OpportunityIntakeApp resource

--- resources/FormAssembly.py
+++ resources/FormAssembly.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, request
 from models.base_model import db
 from models.program_contact_model import ProgramContact, ProgramContactSchema
-from models.opportunity_model import OpportunitySchema
+from models.opportunity_model import OpportunitySchema, OpportunityStage
 from models.program_model import Program
 from models.review_model import Review
 from .Trello_Intake_Talent import get_intake_talent_board_id
@@ -18,21 +18,43 @@ from .trello_utils import (
 opportunity_schema = OpportunitySchema()
 program_contact_schema = ProgramContactSchema()
 
+def get_matching_opp_board_id(program_id):
+    program = Program.query.get(program_id)
+    return program.current_cycle.match_opp_board_id
+
 def get_review_talent_board_id(program_id):
     program = Program.query.get(program_id)
     return program.current_cycle.review_talent_board_id
 
+def find_opp_card(form_data):
+    board_id = get_matching_opp_board_id(form_data['program_id'])
+    board_data = query_board_data(board_id)
+    board = Board(board_data)
+    submitted_list = board.lists['stage'][1]
+    gdoc_id = form_data['google_doc_id']
+    opp_card = board.find_card_by_custom_field('Google Doc ID', gdoc_id)
+    return opp_card, board
+
 class OpportunityIntakeApp(Resource):
     def post(self):
-        from pprint import pprint
         form_data = request.form
+        opp_card, board = find_opp_card(form_data)
+        if opp_card is None:
+            return {'message': 'No card found'}, 404
         opp_data = {
             'title': form_data['title'],
             'short_description': '',
             'gdoc_id': form_data['google_doc_id'],
+            'card_id': opp_card.id,
+            'stage': OpportunityStage.submitted.value
         }
-        opportunity = create_new_opportunity(opp_data)
-        result = opportunity_schema.dump(opportunity)
+        opp = create_new_opportunity(opp_data)
+        opp_card.set_custom_field_values(**{'Opp ID': opp.id})
+        # TODO: Figure out how to handle error values
+        started_list = board.lists['stage'][0]
+        submitted_list = board.lists['stage'][1]
+        opp_card.checked_move_card(started_list, submitted_list)
+        result = opportunity_schema.dump(opp)
         return {"status": 'success', 'data': result}, 201
 
 
