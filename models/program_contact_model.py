f44028026c0f41716cd4500dca52c02af7a9824f
Billy Daly <william.daly@mail.goucher.edu>
2020-02-20 02:54:48 -0500

    Fixes the update() method on program_contact

--- models/program_contact_model.py
+++ models/program_contact_model.py
@@ -31,7 +31,8 @@ class ProgramContact(db.Model):
     # https://medium.com/@s.azad4/modifying-python-objects-within-the-sqlalchemy-framework-7b6c8dd71ab3
     def update(self, **update_dict):
         for field, value in update_dict.items():
-            setattr(self, field, value)
+            if field in UPDATE_FIELDS:
+                setattr(self, field, value)
         db.session.commit()
 
 class ProgramContactSchema(Schema):
