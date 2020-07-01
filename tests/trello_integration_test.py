ac380c38b1f8569a22b1378a15f06c3384c83853
widal001 <william.daly@mail.goucher.edu>
2020-07-01 12:41:18 -0400

    Removes assert True==False from test

--- tests/trello_integration_test.py
+++ tests/trello_integration_test.py
@@ -190,10 +190,8 @@ def test_post_contact(app):
         contact = Contact.query.filter_by(account_id='test-valid|0123456789').first()
         assert contact.first_name == 'Tester'
         assert contact.email == 'testerb@example.com'
-        print(contact.email)
 
         assert UserSession.query.filter_by(contact_id=contact.id).first()
-        assert False == True
 
 # TODO: Add trello specific checks
 def test_post_duplicate_contact(app):
