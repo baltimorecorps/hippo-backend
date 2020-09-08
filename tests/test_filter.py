09682af96f1669f740808966898688325cda914b
widal001 <william.daly@mail.goucher.edu>
2020-09-08 11:46:31 -0400

    Adds filter for the fields stored directly in the `profile` table: - Adds `city` and `state` to the response body for `POST api/contacts/filter` - Accepts fields stored on tables related to the `profile` as filter criteria:     - `programs_completed`     - `roles`

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -16,6 +16,8 @@ OUTPUT = {
         'years_exp': '3-5',
         'job_search_status': 'Actively looking',
         'gender': 'Male',
+        'city': 'Baltimore',
+        'state': 'Maryland'
     }],
     'filter1': [{
         'id': 123,
@@ -27,13 +29,15 @@ OUTPUT = {
         'years_exp': 'filter1',
         'job_search_status': 'filter1',
         'gender': 'Male',
+        'city': 'Baltimore',
+        'state': 'Maryland'
     }],
-    'filter2': []
+    'empty': []
 }
 
 QUERIES = {
     'default': {},
-    'filter1': {
+    'profile_filter1': {
         'status': ['approved'],
         'years_exp': ['filter1'],
         'job_search_status': ['filter1'],
@@ -42,8 +46,29 @@ QUERIES = {
         'previous_bcorps_program': ['filter1'],
         'hear_about_us': ['filter1']
     },
-    'filter2': {
+    'profile_filter2': {
         'years_exp': ['filter2'],
+    },
+    'program_filter1': {
+        'programs_completed': {
+            'mayoral_fellowship': True,
+        }
+    },
+    'program_filter2': {
+        'programs_completed': {
+            'mayoral_fellowship': False,
+        }
+    },
+    'role_filter1': {
+        'roles': {
+            'advocacy_public_policy': True
+        }
+    },
+    'role_filter2': {
+        'roles': {
+            'advocacy_public_policy': True,
+            'community_engagement_outreach': False
+        }
     }
 }
 
@@ -81,15 +106,18 @@ UPDATES = {
         },
         'skills': {}
     },
-    'filter2': {},
 }
 
 
 @pytest.mark.parametrize(
     "update,query,response",
     [(UPDATES['default'], QUERIES['default'], OUTPUT['default']),
-     (UPDATES['filter1'], QUERIES['filter1'], OUTPUT['filter1']),
-     (UPDATES['filter2'], QUERIES['filter2'], OUTPUT['filter2'])]
+     (UPDATES['filter1'], QUERIES['profile_filter1'], OUTPUT['filter1']),
+     (UPDATES['default'], QUERIES['profile_filter2'], OUTPUT['empty']),
+     (UPDATES['default'], QUERIES['program_filter1'], OUTPUT['default']),
+     (UPDATES['default'], QUERIES['program_filter2'], OUTPUT['empty']),
+     (UPDATES['default'], QUERIES['role_filter1'], OUTPUT['default']),
+     (UPDATES['default'], QUERIES['role_filter2'], OUTPUT['empty']),]
 )
 def test_basic_filter(app, update, query, response):
     mimetype = 'application/json'
