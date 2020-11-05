019739b9c755e3ec7f24825dfd77b2920710ad6b
widal001 <william.daly@mail.goucher.edu>
2020-11-05 11:43:55 -0500

    Adds test_program_app.py

---
+++ tests/api_tests/test_program_app.py
@@ -0,0 +1,24 @@
+import json
+import pytest
+
+#imports test data
+from tests.data.contact_data import CONTACTS_API, EMAILS_API
+from tests.data.skill_data import CONTACT_SKILLS
+from tests.data.profile_data import PROFILES_API
+from tests.data.program_data import PROGRAM_APPS_API
+
+
+class TestContactProgramAppsOne:
+
+    def test_get(self):
+        assert 1
+
+class TestContactProgramAppsAll:
+
+    def test_get(self):
+        assert 1
+
+class TestContactProgramAppsInterested:
+
+    def test_put(self):
+        assert 1
