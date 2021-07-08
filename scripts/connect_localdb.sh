cad43695a1df0e4855937550f9b9bcffce396865
widal001 <williamdaly422@gmail.com>
2021-07-07 23:33:25 -0400

    Removes all script files formerly used in hosting local db

--- scripts/connect_localdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-docker run -it --rm --network hippo-localdb \
-           -e PGPASSWORD=localdbpw postgres \
-    psql -h hippo_localdb -U postgres localdb
--- scripts/generate_jwk.py
+++
@@ -1,6 +0,0 @@
-from jwcrypto import jwk
- 
-if __name__ == '__main__':
-    print(jwk.JWK.generate(kty='RSA', size=2048).export_public())
-    print(jwk.JWK.generate(kty='RSA', size=2048).export_private())
-
